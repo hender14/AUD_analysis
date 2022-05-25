117009151d881d18cc0fcf9f304e71d666f23145
hender14 <work_cup_nakano@yahoo.co.jp>
2022-05-25 21:41:02 +0900

    add: テストパタンの追加

--- keywordapi/app/src/test_app.py
+++ keywordapi/app/src/test_app.py
@@ -6,25 +6,33 @@ import service.gcstorage as gcstorage
 load_dotenv()
 gcstorage.make_keyfile()
 
-#############
-## KEY GET ##
-#############
-def test_key_get_exist():
-    app.config['TESTING'] = True
-    client = app.test_client() 
-    result = client.get('/')
-    assert b'root' == result.data
-
-def test_key_get_status():
-    app.config['TESTING'] = True
-    client = app.test_client() 
-    result = client.get('/')
-    assert result.status_code == 200
+###############
+## TEST CASE ##
+###############
+def test_analysis_get_status():
+	app.config['TESTING'] = True
+	client = app.test_client()
+	result = client.post('/analysis', json={'username': os.environ['TESTUSER_ID'], 'filename': os.environ['TESTUSER_TRANSLATE_FILENAME']})
+	assert result.status_code == 200
 
 def test_list_get_status():
-    app.config['TESTING'] = True
-    client = app.test_client()
-    result = client.get('/list', query_string={'username': os.environ['TESTUSER_ID']})
-    # ans = {'name': ['1650899873639-h4b_serverless_4.json'], 'generation': [1650899887475]}
-    assert result.status_code == 200
-    # assert ans == result.json
+	app.config['TESTING'] = True
+	client = app.test_client()
+	result = client.get('/list', query_string={'username': os.environ['TESTUSER_ID']})
+	ans = [os.environ['TESTUSER_ANALYSIS_FILENAME']]
+	assert result.status_code == 200
+	assert ans == result.json["name"]
+
+
+def test_detail_get_status():
+	app.config['TESTING'] = True
+	client = app.test_client()
+	result = client.get('/detail', query_string={'username': os.environ['TESTUSER_ID'], 'filename': os.environ['TESTUSER_ANALYSIS_FILENAME']})
+	print(result.json)
+	assert result.status_code == 200
+
+def test_delete_get_status():
+	app.config['TESTING'] = True
+	client = app.test_client()
+	result = client.get('/delete', query_string={'username': os.environ['TESTUSER_ID'], 'filename': os.environ['TESTUSER_ANALYSIS_FILENAME']})
+	assert result.status_code == 200
