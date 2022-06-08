108493a9ccfc1cb8354e3f77e126402513a10847
hender14 <work_cup_nakano@yahoo.co.jp>
2022-06-09 07:25:37 +0900

    add: contact api

--- loginapi/app/controller/contactController.go
+++ loginapi/app/controller/contactController.go
@@ -11,9 +11,9 @@ import (
 )
 
 func Contact(ctx *gin.Context) {
-	var data map[string]string
+	var input model.CntmailPara
 	// parse request data
-	if err := ctx.BindJSON(&data); err != nil {
+	if err := ctx.BindJSON(&input); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Tt has problem for input data"})
 		return
 	}
@@ -22,21 +22,21 @@ func Contact(ctx *gin.Context) {
 	mailconfig := Contactmail()
 	// fmt.Printf("mailconfig: %+v\n", mailconfig)
 	// fmt.Printf("data: %+v\n", data)
-	_, err := service.Sendmail(mailconfig, data, "contact")
+	_, err := service.Cnt_Sendmail(mailconfig, input)
 	if err != nil {
 		// errmessage := "firebase auth has problem: " + err
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err })
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": err})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H {"message": "contact has completed"})
+	ctx.JSON(http.StatusOK, gin.H{"message": "contact has completed"})
 }
 
-func Contactmail() ( model.Mails ) {
+func Contactmail() model.Mails {
 	mailconfig := model.Mails{
-		From: model.Sendaddress{ Express: "AUD Contact", Address: os.Getenv("SENDGRID_FROM_EMAIL")},
-		To: model.Sendaddress{ Express: "Owner", Address:os.Getenv("SENDGRID_FROM_EMAIL")},
+		From:    model.Sendaddress{Express: "AUD Contact", Address: os.Getenv("SENDGRID_FROM_EMAIL")},
+		To:      model.Sendaddress{Express: "Owner", Address: os.Getenv("SENDGRID_FROM_EMAIL")},
 		Subject: "Contact Owner",
-  }
+	}
 	return mailconfig
 }
--- loginapi/app/service/sendgrid.go
+++ loginapi/app/service/sendgrid.go
@@ -2,8 +2,9 @@ package service
 
 import (
 	"encoding/json"
-	"log"
+	// "log"
 	"os"
+	// "testing"
 
 	"app/model"
 
@@ -11,68 +12,30 @@ import (
 	"github.com/sendgrid/sendgrid-go"
 )
 
-// SendGridｻｰﾊﾞよりﾒｰﾙの送付
-func Sendmail(config model.Mails, param map[string]string, state string) (response *rest.Response, err error) {
-	var mail any
-	// SendGridのmail config
-	if (state == "reset") {
-		mail = RstSGConfig(config, param)
-	} else {
-		mail = CntSGConfig(config, param)
-	}
-	// fmt.Printf("mail: %+v\n", mail)
-	// GoのｺｰﾄﾞをJSON化
-	jsondata, err := json.Marshal(mail)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// SendGridのrequest config
-	request := ReqConfig()
-	// JSON化したmailの内容をrequest.Bodyに代入
-	request.Body = jsondata
-
-	// sendGridのAPIにﾘｸｴｽﾄをｾｯﾄ, 戻り値でresponseが返ってくる
-	response, err = sendgrid.API(request)
-	if err != nil {
-			log.Println(err)
-	} else {
-			// fmt.Println(response.StatusCode)
-			// fmt.Println(response.Body)
-			// fmt.Println(response.Headers)
-	}
-	return response, err
+type RstSGmail struct {
+	Mail    *model.RstMail
+	Jsndata []byte
+	Request rest.Request
 }
 
-func ReqConfig() ( rest.Request ) {
-	apikey := os.Getenv("SENDGRID_API_KEY")
-	// ﾎｽﾄ
-	host := "https://api.sendgrid.com"
-	// ｴﾝﾄﾞﾎﾟｲﾝﾄ
-	endpoint := "/v3/mail/send"
-	// API KEYとｴﾝﾄﾞﾎﾟｲﾝﾄ、ﾎｽﾄからrestﾊﾟｯｹｰｼﾞのRequestを生成
-	request := sendgrid.GetRequest(apikey, endpoint, host)
-	// requestのMethodをPostに
-	request.Method = "POST"
-
-	return request
+type CntSGmail struct {
+	Mail    *model.CntMail
+	Jsndata []byte
+	Request rest.Request
 }
 
-func RstSGConfig(config model.Mails, param map[string]string) ( model.RstMail ) {
-	templateId := os.Getenv("SENDGRID_RST_TEMPLATED_ID")
-  // ﾒｰﾙの内容をJSONで作成する
-	subject := config.Subject
-
-	mail := model.RstMail{
-		Subject: subject,
+func (r *RstSGmail) SGConf(config model.Mails, param model.RstmailPara) (err error) {
+	// Mailﾃﾞｰﾀを作成
+	r.Mail = &model.RstMail{
+		Subject: config.Subject,
 		Personalizations: []model.RstPersonal{
 			{To: []model.MailUser{{
 				Email: config.To.Address,
 				Name:  config.To.Express,
 			}},
 			Parameter: model.RstmailPara{
-				Reseturl: param["reseturl"],
-				Email: param["email"],
+				Reseturl: param.Reseturl,
+				Email: param.Email,
 			},
 			},
 		},
@@ -80,28 +43,41 @@ func RstSGConfig(config model.Mails, param map[string]string) ( model.RstMail ) 
 			Email: config.From.Address,
 			Name:  config.From.Express,
 		},
-		TemplateId: templateId,
+		TemplateId: os.Getenv("SENDGRID_RST_TEMPLATED_ID"),
 	}
-	return mail
+
+	// r.Mail = new(model.RstMail)
+	// r.Mail.Subject = config.Subject
+	// r.Mail.Personalizations[0].To[0].Email = config.To.Address
+	// r.Mail.Personalizations[0].To[0].Name = config.To.Express
+	// r.Mail.Personalizations[0].Parameter.Reseturl = param.Reseturl
+	// r.Mail.Personalizations[0].Parameter.Email = param.Email
+	// r.Mail.From.Email = config.From.Address
+	// r.Mail.From.Name = config.From.Express
+	// r.Mail.TemplateId = os.Getenv("SENDGRID_RST_TEMPLATED_ID")
+
+	// MailデータをJSON化
+	r.Jsndata, err = json.Marshal(&(r.Mail))
+	if err != nil {
+		return err
+	}
+	return err
 }
 
-func CntSGConfig(config model.Mails, param map[string]string) ( model.CntMail ) {
-	templateId := os.Getenv("SENDGRID_CNT_TEMPLATED_ID")
-  // ﾒｰﾙの内容をJSONで作成する
-	subject := config.Subject
-
-	mail := model.CntMail{
-		Subject: subject,
+func (C *CntSGmail) SGConf(config model.Mails, param model.CntmailPara) (err error) {
+	// Mailデータを作成
+	C.Mail =  &model.CntMail{
+		Subject: config.Subject,
 		Personalizations: []model.CntPersonal{
 			{To: []model.MailUser{{
 				Email: config.To.Address,
 				Name:  config.To.Express,
 			}},
 			Parameter: model.CntmailPara{
-				ID: param["ID"],
-				Title: param["title"],
-				Content: param["content"],
-				Email: param["email"],
+				ID: param.ID,
+				Title: param.Title,
+				Content: param.Content,
+				Email: param.Email,
 			},
 			},
 		},
@@ -109,7 +85,163 @@ func CntSGConfig(config model.Mails, param map[string]string) ( model.CntMail ) 
 			Email: config.From.Address,
 			Name:  config.From.Express,
 		},
-		TemplateId: templateId,
+		TemplateId: os.Getenv("SENDGRID_CNT_TEMPLATED_ID"),
 	}
-	return mail
+
+	// C.Mail.Subject = config.Subject
+	// C.Mail.Personalizations[0].To[0].Email = config.To.Address
+	// C.Mail.Personalizations[0].To[0].Name = config.To.Express
+	// C.Mail.Personalizations[0].Parameter.ID = param.ID
+	// C.Mail.Personalizations[0].Parameter.Title = param.Title
+	// C.Mail.Personalizations[0].Parameter.Content = param.Content
+	// C.Mail.Personalizations[0].Parameter.Email = param.Email
+	// C.Mail.From.Email = config.From.Address
+	// C.Mail.From.Name = config.From.Express
+	// C.Mail.TemplateId = os.Getenv("SENDGRID_CNT_TEMPLATED_ID")
+
+	// MailデータをJSON化
+	C.Jsndata, err = json.Marshal(&(C.Mail))
+	if err != nil {
+		return err
+	}
+	return err
 }
+
+func (r *RstSGmail) Reqconf() (response *rest.Response, err error) {
+	// requestのMethodをPostに
+	r.Request.Method = "POST"
+	r.Request.Body = r.Jsndata
+	response, err = sendgrid.API(r.Request)
+	// fmt.Println(response.StatusCode)
+	// fmt.Println(response.Body)
+	// fmt.Println(response.Headers)
+	return response, err
+}
+
+func (c *CntSGmail) Reqconf() (response *rest.Response, err error) {
+	// requestのMethodをPostに
+	c.Request.Method = "POST"
+	c.Request.Body = c.Jsndata
+	response, err = sendgrid.API(c.Request)
+	// fmt.Println(response.StatusCode)
+	// fmt.Println(response.Body)
+	// fmt.Println(response.Headers)
+	return response, err
+}
+
+func Rst_NewRequest() (r *RstSGmail) {
+	r = new(RstSGmail)
+	apikey := os.Getenv("SENDGRID_API_KEY")
+	// ﾎｽﾄ
+	host := "https://api.sendgrid.com"
+	// ｴﾝﾄﾞﾎﾟｲﾝﾄ
+	endpoint := "/v3/mail/send"
+	// API KEYとｴﾝﾄﾞﾎﾟｲﾝﾄ、ﾎｽﾄからrestﾊﾟｯｹｰｼﾞのRequestを生成
+	r.Request = sendgrid.GetRequest(apikey, endpoint, host)
+
+	return r
+}
+
+func Cnt_NewRequest() (c *CntSGmail) {
+	c = new(CntSGmail)
+	apikey := os.Getenv("SENDGRID_API_KEY")
+	// ﾎｽﾄ
+	host := "https://api.sendgrid.com"
+	// ｴﾝﾄﾞﾎﾟｲﾝﾄ
+	endpoint := "/v3/mail/send"
+	// API KEYとｴﾝﾄﾞﾎﾟｲﾝﾄ、ﾎｽﾄからrestﾊﾟｯｹｰｼﾞのRequestを生成
+	c.Request = sendgrid.GetRequest(apikey, endpoint, host)
+
+	return c
+}
+
+// SendGridｻｰﾊﾞよりﾒｰﾙの送付
+func Sendmail(config model.Mails, param model.RstmailPara) (response *rest.Response, err error) {
+	R := Rst_NewRequest()
+	// SendGridのmail config
+	err = R.SGConf(config, param)
+	// fmt.Printf("mail: %+v\n", mail)
+	if err != nil {
+		return nil, err
+	}
+
+	// sendGridのAPIにﾘｸｴｽﾄをｾｯﾄ, 戻り値でresponseが返ってくる
+	response, err = R.Reqconf()
+	if err != nil {
+		return nil, err
+	}
+	return response, err
+}
+
+func Cnt_Sendmail(config model.Mails, param model.CntmailPara) (response *rest.Response, err error) {
+	C := Cnt_NewRequest()
+	// SendGridのmail config
+	err = C.SGConf(config, param)
+	// fmt.Printf("mail: %+v\n", mail)
+	if err != nil {
+		return nil, err
+	}
+
+	// sendGridのAPIにﾘｸｴｽﾄをｾｯﾄ, 戻り値でresponseが返ってくる
+	response, err = C.Reqconf()
+	if err != nil {
+		return nil, err
+	}
+	return response, err
+}
+
+// func CntSGConfig(config model.Mails, param map[string]string) ( model.CntMail ) {
+// 	templateId := os.Getenv("SENDGRID_CNT_TEMPLATED_ID")
+//   // ﾒｰﾙの内容をJSONで作成する
+// 	subject := config.Subject
+
+// 	mail := model.CntMail{
+// 		Subject: subject,
+// 		Personalizations: []model.CntPersonal{
+// 			{To: []model.MailUser{{
+// 				Email: config.To.Address,
+// 				Name:  config.To.Express,
+// 			}},
+// 			Parameter: model.CntmailPara{
+// 				ID: param["ID"],
+// 				Title: param["title"],
+// 				Content: param["content"],
+// 				Email: param["email"],
+// 			},
+// 			},
+// 		},
+// 		From: model.MailUser{
+// 			Email: config.From.Address,
+// 			Name:  config.From.Express,
+// 		},
+// 		TemplateId: templateId,
+// 	}
+// 	return mail
+// }
+
+// func RstSGConfig(config model.Mails, param map[string]string) ( model.RstMail ) {
+// 	templateId := os.Getenv("SENDGRID_RST_TEMPLATED_ID")
+//   // ﾒｰﾙの内容をJSONで作成する
+// 	subject := config.Subject
+
+// 	mail := model.RstMail{
+// 		Subject: subject,
+// 		Personalizations: []model.RstPersonal{
+// 			{To: []model.MailUser{{
+// 				Email: config.To.Address,
+// 				Name:  config.To.Express,
+// 			}},
+// 			Parameter: model.RstmailPara{
+// 				Reseturl: param["reseturl"],
+// 				Email: param["email"],
+// 			},
+// 			},
+// 		},
+// 		From: model.MailUser{
+// 			Email: config.From.Address,
+// 			Name:  config.From.Express,
+// 		},
+// 		TemplateId: templateId,
+// 	}
+// 	return mail
+// }
